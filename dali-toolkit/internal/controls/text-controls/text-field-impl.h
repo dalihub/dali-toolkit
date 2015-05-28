eff9a302f2e5999932743f5d5341e315f627793e
Victor Cebollada <v.cebollada@samsung.com>
2015-05-28 15:45:13 +0100

    Fixes for the text controller.
    Triggers a size negotiation or resets the renderable actor when properties change.
    
    Change-Id: Idaf3cc172deb4f07f7245dee06755d4ae821c03a
    Signed-off-by: Victor Cebollada <v.cebollada@samsung.com>

--- dali-toolkit/internal/controls/text-controls/text-field-impl.h
+++ dali-toolkit/internal/controls/text-controls/text-field-impl.h
@@ -18,9 +18,6 @@
  *
  */
 
-// EXTERNAL INCLUDES
-#include <dali/devel-api/adaptor-framework/imf-manager.h>
-
 // INTERNAL INCLUDES
 #include <dali-toolkit/public-api/controls/control-impl.h>
 #include <dali-toolkit/public-api/controls/text-controls/text-field.h>
@@ -140,11 +137,7 @@ private: // From Control
   virtual bool OnKeyEvent(const KeyEvent& event);
 
   /**
-   * @brief Event received from IMF manager
-   *
-   * @param[in] imfManager The IMF manager.
-   * @param[in] imfEvent The event received.
-   * @return A data struture indicating if update is needed, cursor position and current text.
+   * @copydoc Dali::Toolkit::Text::Controller::(ImfManager& imfManager, const ImfManager::ImfEventData& imfEvent)
    */
   ImfManager::ImfCallbackData OnImfEvent( ImfManager& imfManager, const ImfManager::ImfEventData& imfEvent );
 
