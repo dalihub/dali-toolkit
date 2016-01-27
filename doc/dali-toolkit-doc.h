e2c0ad1573b105352c195cad70429432f400f58d
Yoonsang Lee <ysang114.lee@samsung.com>
2016-01-28 08:11:57 +0900

    Update doxygen groups and overview description
    - Merge "Focus Manager" & "Accessibility Manager" groups into "Managers"
    - Update overview of DALi
    - Update overview of DALi Toolkit
    
    Change-Id: Iad7d693276b1c13274305a0c73b7ca49e54d9dc4

--- doc/dali-toolkit-doc.h
+++ doc/dali-toolkit-doc.h
@@ -1,6 +1,23 @@
 #ifndef __DALI_TOOLKIT_DOC_H__
 #define __DALI_TOOLKIT_DOC_H__
 
+/*
+ * Copyright (c) 2016 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 /**
  * @defgroup dali DALi
  * @ingroup CAPI_UI_FRAMEWORK
@@ -8,26 +25,101 @@
  * @brief DALi is a cross-platform 3D UI Toolkit for embedded systems.
  *
  * @section dali-overview Overview
+ *
  * DALi's 3D user interface engine enables you to create rich and high-performance
  * UI applications. DALi is based on OpenGL ES 2.0, but provides a clean
  * cross-platform C++ framework.
  * This means that you can use high-level DALi APIs instead of accessing
  * low-level OpenGL APIs directly.
- * <ul>
- * <li>DALi Core: This module provides scene graph-based rendering, animation, and event handling.</li>
- * <li>DALi Adaptor: This module is a platform adaptation layer.</li>
- * <li>DALi Toolkit: This module provides UI components and various effects on top of the DALi Core.</li>
- * </ul>
+ *
+ * DALi consists of the following modules:
+ * <table>
+ * <tr>
+ *    <th>Module</th>
+ *    <th>Description</th>
+ * </tr>
+ * <tr>
+ *    <td>@ref dali_core</td>
+ *    <td>DALi Core provides core functionalities such as scene graph-based rendering, animation, and event handling.</td>
+ * </tr>
+ * <tr>
+ *    <td>@ref dali_adaptor</td>
+ *    <td>DALi Adaptor is a platform adaptation layer.</td>
+ * </tr>
+ * <tr>
+ *    <td>@ref dali_toolkit</td>
+ *    <td>DALi Toolkit provides UI components and various effects on top of the DALi Core.</td>
+ * </tr>
+ * </table>
+ *
+ * The layer diagram for DALi modules is shown below:
+ * @image html dali-modules.png "Figure: DALi modules"
  *
  * @defgroup dali_toolkit DALi Toolkit
+ *
+ * @brief DALi Toolkit provides UI components and various effects on top of the DALi Core.
+ *
+ * @section dali_core_overview Overview
+ *
+ * DALi Toolkit consists of the following groups of API:
+ *
+ * <table>
+ * <tr>
+ *    <th>API Group</th>
+ *    <th>Description</th>
+ * </tr>
+ * <tr>
+ *    <td>@ref dali_toolkit_controls</td>
+ *    <td>Controls are interactive components for layouting the user interface.</td>
+ * </tr>
+ * <tr>
+ *    <td style="padding-left:1em">@ref dali_toolkit_controls_alignment</td>
+ *    <td>Alignment is a container which provides an easy way to align other actors inside its boundary.</td>
+ * </tr>
+ * <tr>
+ *    <td style="padding-left:1em">@ref dali_toolkit_controls_buttons</td>
+ *    <td>Button is a small object on UI that you can press.</td>
+ * </tr>
+ * <tr>
+ *    <td style="padding-left:1em">@ref dali_toolkit_controls_gaussian_blur_view</td>
+ *    <td>GaussianBlurView provides a render process that blurs an image.</td>
+ * </tr>
+ * <tr>
+ *    <td style="padding-left:1em">@ref dali_toolkit_controls_image_view</td>
+ *    <td>ImageView is a control displying an image.</td>
+ * </tr>
+ * <tr>
+ *    <td style="padding-left:1em">@ref dali_toolkit_controls_scroll_bar</td>
+ *    <td>ScrollBar control.</td>
+ * </tr>
+ * <tr>
+ *    <td style="padding-left:1em">@ref dali_toolkit_controls_scrollable</td>
+ *    <td>Scrollable container controls.</td>
+ * </tr>
+ * <tr>
+ *    <td style="padding-left:2em">@ref dali_toolkit_controls_item_view</td>
+ *    <td>ItemView class is a scrollable container that can contain many items.</td>
+ * </tr>
+ * <tr>
+ *    <td style="padding-left:2em">@ref dali_toolkit_controls_scroll_view</td>
+ *    <td>ScrollView class provides scrollable view which contains actors and can be scrolled automatically or manually by panning.</td>
+ * </tr>
+ * <tr>
+ *    <td style="padding-left:1em">@ref dali_toolkit_controls_table_view</td>
+ *    <td>TableView class is a layout container for aligning child actors in a grid like layout.</td>
+ * </tr>
+ * <tr>
+ *    <td style="padding-left:1em">@ref dali_toolkit_controls_text_controls</td>
+ *    <td>Controls for displaying text or text input.</td>
+ * </tr>
+ * <tr>
+ *    <td>@ref dali_toolkit_managers</td>
+ *    <td>Singleton classes for managing application-wide functionalities.</td>
+ * </tr>
+ * </table>
+ *
  * @ingroup dali
- *
- * @brief This module provides UI components and various effects on top of the DALi Core.
- *
  * @{
- *   @defgroup dali_toolkit_accessibility_manager Accessibility Manager
- *   @brief AccessibilityManager manages a accessibility focus chain.
-
  *   @defgroup dali_toolkit_controls Controls
  *   @brief Controls are interactive components for layouting the user interface.
 
@@ -65,8 +157,9 @@
  *     @brief Controls for displaying text or text input.
 
  *   @}
- *   @defgroup dali_toolkit_focus_manager Focus Manager
- *   @brief Classes for handling keyboard navigation and maintaining the two dimensional keyboard focus chain.
+
+ *   @defgroup dali_toolkit_managers Managers
+ *   @brief Singleton classes for managing application-wide functionalities.
 
  * @}
  */
