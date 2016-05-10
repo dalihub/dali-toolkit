57a562d0bcf8565327e0bad1750f1628d551fe0d
Seoyeon Kim <seoyeon2.kim@samsung.com>
2016-05-10 16:43:11 +0900

    Change 'char' type to 'signed char'
    - In some build environments, like ARM architecture, 'char' without a signedness
      qualifier can be interpreted as 'unsigned char', and it may result in
      unexpected behavior.
    
    Change-Id: Ib362f914f853638a954dc0f39e9b6fb19f7043a0
    Signed-off-by: Seoyeon Kim <seoyeon2.kim@samsung.com>

--- dali-toolkit/internal/controls/renderers/svg/nanosvg/nanosvg.h
+++ dali-toolkit/internal/controls/renderers/svg/nanosvg/nanosvg.h
@@ -113,7 +113,12 @@ typedef struct NSVGgradient {
 } NSVGgradient;
 
 typedef struct NSVGpaint {
-	char type;
+	/**
+	 * In the original file, using char type (without signed or unsigned) can be interpreted
+	 * as 'unsigned char' in some build environments, like ARM architecture.
+	 * To prevent the unexpected behavior, we replace 'char type' with 'signed char type' here.
+	 */
+	signed char type;
 	union {
 		unsigned int color;
 		NSVGgradient* gradient;
