761692296156a98d2cc2b41790a220bf830b6e55
taeyoon <taeyoon0.lee@samsung.com>
2016-08-02 18:24:40 +0900

    [3.0] Add GetSurfaceType() in TestRenderSurface
      GetSurfaceType() will be added for NativeRenderSurface
    
    Change-Id: I577433cd42c2db8b765b66ef4ef496052a11fef4

--- automated-tests/src/dali-toolkit/dali-toolkit-test-utils/toolkit-adaptor.cpp
+++ automated-tests/src/dali-toolkit/dali-toolkit-test-utils/toolkit-adaptor.cpp
@@ -64,6 +64,7 @@ public:
 
   virtual void SetThreadSynchronization( ThreadSynchronizationInterface& threadSynchronization ) {}
 
+  virtual RenderSurface::Type GetSurfaceType() { return RenderSurface::ECORE_RENDER_SURFACE; }
 };
 
 namespace Internal
