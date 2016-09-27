5343a13f0034d152c814eef9d68b83d14b4c8997
taeyoon <taeyoon0.lee@samsung.com>
2016-09-27 17:58:37 +0900

    [3.0] Add GetEglImageTextureTarget() in TestNativeImageExtension
    Change-Id: Ibde5be67dc31a93bb680d897d1ed20af205fd189

--- automated-tests/src/dali-toolkit/dali-toolkit-test-utils/test-native-image.h
+++ automated-tests/src/dali-toolkit/dali-toolkit-test-utils/test-native-image.h
@@ -19,8 +19,11 @@
  */
 
 // INTERNAL INCLUDES
+
+// EXTERNAL INCLUDES
 #include <dali/public-api/images/native-image-interface.h>
 #include <dali/devel-api/images/native-image-interface-extension.h>
+#include <dali/integration-api/gl-defines.h>
 
 namespace Dali
 {
@@ -32,6 +35,8 @@ class DALI_IMPORT_API TestNativeImageExtension: public Dali::NativeImageInterfac
 public:
   inline const char* GetCustomFragmentPreFix(){return "#extension GL_OES_EGL_image_external:require\n";}
   inline const char* GetCustomSamplerTypename(){return "samplerExternalOES";}
+
+  inline int GetEglImageTextureTarget(){return GL_TEXTURE_2D;}
 
 };
 
