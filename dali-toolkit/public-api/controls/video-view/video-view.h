c1481543c3334342110f9a4635292d0eff1d15b7
Seoyeon Kim <seoyeon2.kim@samsung.com>
2017-02-07 14:54:43 +0900

    Update doxygen comments
    - For document public release, Native API reference is checked by Lionbridge.
    
    Change-Id: I78cad260974f386ceeeb1fbc8774815be2213457
    Signed-off-by: Seoyeon Kim <seoyeon2.kim@samsung.com>

--- dali-toolkit/public-api/controls/video-view/video-view.h
+++ dali-toolkit/public-api/controls/video-view/video-view.h
@@ -67,7 +67,7 @@ public:
   typedef Signal< void (VideoView&) > VideoViewSignalType; ///< Video playback finished signal type @ SINCE_1_1.38
 
   /**
-   * @brief The start and end property ranges for this control.
+   * @brief Enumeration for the start and end property ranges for this control.
    * @SINCE_1_0.0
    */
   enum PropertyRange
@@ -80,25 +80,25 @@ public:
     enum
     {
       /**
-       * @brief name "video", video file url as string type or Property::Map
+       * @brief name "video", video file url as string type or Property::Map.
        * @SINCE_1_1.38
        */
       VIDEO = PROPERTY_START_INDEX,
 
      /**
-       * @brief name "looping", looping status, true or false
+       * @brief name "looping", looping status, true or false.
        * @SINCE_1_1.38
        */
       LOOPING,
 
      /**
-       * @brief name "muted", mute status, true or false
+       * @brief name "muted", mute status, true or false.
        * @SINCE_1_1.38
        */
       MUTED,
 
      /**
-       * @brief name "volume", left and right volume scalar as float type, Property::Map with two values ( "left" and "right" )
+       * @brief name "volume", left and right volume scalar as float type, Property::Map with two values ( "left" and "right" ).
        * @SINCE_1_1.38
        */
       VOLUME
@@ -118,7 +118,7 @@ public:
 
   /**
    * @brief Creates an initialized VideoView.
-   * If the string is empty, VideoView will not display anything
+   * If the string is empty, VideoView will not display anything.
    *
    * @SINCE_1_1.38
    * @REMARK_INTERNET
@@ -129,21 +129,21 @@ public:
   static VideoView New( const std::string& url );
 
   /**
-   * @brief Create an uninitialized VideoView
+   * @brief Creates an uninitialized VideoView.
    * @SINCE_1_1.38
    */
   VideoView();
 
   /**
-   * @brief Destructor
+   * @brief Destructor.
    *
-   * This is non-virtual since derived Handel types must not contain data or virtual methods
+   * This is non-virtual since derived Handel types must not contain data or virtual methods.
    * @SINCE_1_1.38
    */
   ~VideoView();
 
   /*
-   * @brief Copy constructor
+   * @brief Copy constructor.
    *
    * @SINCE_1_1.38
    * @param[in] videoView VideoView to copy. The copied VideoView will point at the same implementation
@@ -154,20 +154,20 @@ public:
    * @brief Assignment operator.
    *
    * @SINCE_1_1.38
-   * @param[in] videoView The VideoView to assign from.
-   * @return The updated VideoView.
+   * @param[in] videoView The VideoView to assign from
+   * @return The updated VideoView
    */
   VideoView& operator=( const VideoView& videoView );
 
   /**
-   * @brief Downcast a handle to VideoView handle.
+   * @brief Downcasts a handle to VideoView handle.
    *
-   * If handle points to a VideoView the downcast produces valid
-   * handle. If not the returned handle is left uninitialized.
+   * If handle points to a VideoView, the downcast produces valid handle.
+   * If not, the returned handle is left uninitialized.
    *
    * @SINCE_1_1.38
    * @param[in] handle Handle to an object
-   * @return handle to a VideoView or an uninitialized handle
+   * @return Handle to a VideoView or an uninitialized handle
    */
   static VideoView DownCast( BaseHandle handle );
 
@@ -206,10 +206,10 @@ public:
   void Backward( int millisecond );
 
   /**
-   * @brief Connect to this signal to be notified when a video playback have finished.
+   * @brief Connects to this signal to be notified when a video playback is finished.
    *
    * @SINCE_1_1.38
-   * @return A signal object to connect with.
+   * @return A signal object to connect with
    */
   VideoViewSignalType& FinishedSignal();
 
@@ -220,7 +220,7 @@ public: // Not intended for application developers
    * @brief Creates a handle using the Toolkit::Internal implementation.
    *
    * @SINCE_1_1.38
-   * @param[in] implementation The VideoView implementation.
+   * @param[in] implementation The VideoView implementation
    */
   DALI_INTERNAL VideoView( Internal::VideoView& implementation );
 
@@ -228,7 +228,7 @@ public: // Not intended for application developers
    * @brief Allows the creation of this VideoView from an Internal::CustomActor pointer.
    *
    * @SINCE_1_1.38
-   * @param[in] internal A pointer to the internal CustomActor.
+   * @param[in] internal A pointer to the internal CustomActor
    */
   DALI_INTERNAL VideoView( Dali::Internal::CustomActor* internal );
   /// @endcond
