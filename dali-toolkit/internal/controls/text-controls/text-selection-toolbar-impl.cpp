bf844b5a1982a6ff32fd38b8f5aa92672f1aa403
Paul Wisbey <p.wisbey@samsung.com>
2017-03-03 17:39:48 +0000

    [3.0] Fix for TextSelectionToolbar overshootEffectColor styling
    Change-Id: I9ee4b4182964cccdf0b1540e1495fc5d325227f8

--- dali-toolkit/internal/controls/text-controls/text-selection-toolbar-impl.cpp
+++ dali-toolkit/internal/controls/text-controls/text-selection-toolbar-impl.cpp
@@ -29,6 +29,7 @@
 // INTERNAL INCLUDES
 #include <dali-toolkit/public-api/controls/image-view/image-view.h>
 #include <dali-toolkit/devel-api/controls/control-depth-index-ranges.h>
+#include <dali-toolkit/internal/helpers/color-conversion.h>
 
 namespace Dali
 {
@@ -350,9 +351,21 @@ void TextSelectionToolbar::ConfigureScrollview( const Property::Map& properties 
     Property::Index setPropertyIndex = mScrollView.GetPropertyIndex( propertyPair.first );
     if( setPropertyIndex != Property::INVALID_INDEX )
     {
-      // If the conversion worked, we have a valid property index,
-      // Set the property to the new value.
-      mScrollView.SetProperty( setPropertyIndex, propertyPair.second );
+      // Convert the string representation of a color into a Vector4
+      if( setPropertyIndex == Toolkit::Scrollable::Property::OVERSHOOT_EFFECT_COLOR )
+      {
+        Vector4 color;
+        if( ConvertPropertyToColor( propertyPair.second, color ) )
+        {
+          mScrollView.SetOvershootEffectColor( color );
+        }
+      }
+      else
+      {
+        // If the conversion worked, we have a valid property index,
+        // Set the property to the new value.
+        mScrollView.SetProperty( setPropertyIndex, propertyPair.second );
+      }
     }
   }
 
