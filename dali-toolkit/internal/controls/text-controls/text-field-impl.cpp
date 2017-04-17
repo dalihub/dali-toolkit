ea6b12e6d02a45b894dc774776aef2f433ec05f2
Jinho, Lee <jeano.lee@samsung.com>
2017-04-17 13:00:19 +0900

    Revert "[Tizen] Fix the Keyboard disappeared issue in TextField"
    This reverts commit 77e7e88dd16a4aac3ad825d1d3f7105999cff17d.
    
    Change-Id: Icd95707dc814df4c6a662ac0d06e0a2937e9aff3

--- dali-toolkit/internal/controls/text-controls/text-field-impl.cpp
+++ dali-toolkit/internal/controls/text-controls/text-field-impl.cpp
@@ -1429,7 +1429,7 @@ bool TextField::OnKeyEvent( const KeyEvent& event )
 {
   DALI_LOG_INFO( gLogFilter, Debug::Verbose, "TextField::OnKeyEvent %p keyCode %d\n", mController.Get(), event.keyCode );
 
-  if( Dali::DALI_KEY_ESCAPE == event.keyCode ) // Make a Dali key code for this
+  if( Dali::DALI_KEY_ESCAPE == event.keyCode || "Return" == event.keyPressedName ) // Make a Dali key code for this
   {
     // Make sure ClearKeyInputFocus when only key is up
     if( event.state == KeyEvent::Up )
