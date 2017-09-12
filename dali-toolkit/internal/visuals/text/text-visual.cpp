9d9d656c92bc768ef31c0bae604102071e6e3029
Seoyeon Kim <seoyeon2.kim@samsung.com>
2017-09-12 17:03:07 +0900

    Fix SVACE issue
    - Fix to prevent 'name' from passing a null pointer to a 'std::string',
    because it would result in dereferencing a null pointer.
    
    Change-Id: If01974fa77662db75480962ff52373a6cc403b17
    Signed-off-by: Seoyeon Kim <seoyeon2.kim@samsung.com>

--- dali-toolkit/internal/visuals/text/text-visual.cpp
+++ dali-toolkit/internal/visuals/text/text-visual.cpp
@@ -74,7 +74,11 @@ std::string GetHorizontalAlignment( Toolkit::Text::Layout::HorizontalAlignment a
                                                                                                 HORIZONTAL_ALIGNMENT_STRING_TABLE,
                                                                                                 HORIZONTAL_ALIGNMENT_STRING_TABLE_COUNT );
 
-  return std::string( name );
+  if( name )
+  {
+    return std::string( name );
+  }
+  return std::string();
 }
 
 std::string GetVerticalAlignment( Toolkit::Text::Layout::VerticalAlignment alignment )
@@ -83,7 +87,11 @@ std::string GetVerticalAlignment( Toolkit::Text::Layout::VerticalAlignment align
                                                                                                 VERTICAL_ALIGNMENT_STRING_TABLE,
                                                                                                 VERTICAL_ALIGNMENT_STRING_TABLE_COUNT );
 
-  return std::string( name );
+  if( name )
+  {
+    return std::string( name );
+  }
+  return std::string();
 }
 
 const char* VERTEX_SHADER = DALI_COMPOSE_SHADER(
