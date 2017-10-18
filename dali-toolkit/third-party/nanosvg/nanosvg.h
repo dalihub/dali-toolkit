63e9a744a99929598cbf5c13614010a0133fc277
David Steele <david.steele@samsung.com>
2017-10-18 15:42:34 +0100

    [4.0] Updated NanoSVG to latest version (9 July 2017)
    This fixes a number of issues, including the arc drawing.
    See https://github.com/memononen/nanosvg/commits/master for further details
    
    Change-Id: I9f5bff91167c2dea625efa903a18f9292c3853b5

--- dali-toolkit/third-party/nanosvg/nanosvg.h
+++ dali-toolkit/third-party/nanosvg/nanosvg.h
@@ -48,116 +48,117 @@
 
 
 /* Example Usage:
-	// Load
-	SNVGImage* image;
-	image = nsvgParseFromFile("test.svg", "px", 96);
-	printf("size: %f x %f\n", image->width, image->height);
-	// Use...
-	for (shape = image->shapes; shape != NULL; shape = shape->next) {
-		for (path = shape->paths; path != NULL; path = path->next) {
-			for (i = 0; i < path->npts-1; i += 3) {
-				float* p = &path->pts[i*2];
-				drawCubicBez(p[0],p[1], p[2],p[3], p[4],p[5], p[6],p[7]);
-			}
-		}
-	}
-	// Delete
-	nsvgDelete(image);
+  // Load
+  SNVGImage* image;
+  image = nsvgParseFromFile("test.svg", "px", 96);
+  printf("size: %f x %f\n", image->width, image->height);
+  // Use...
+  for (shape = image->shapes; shape != NULL; shape = shape->next) {
+    for (path = shape->paths; path != NULL; path = path->next) {
+      for (i = 0; i < path->npts-1; i += 3) {
+        float* p = &path->pts[i*2];
+        drawCubicBez(p[0],p[1], p[2],p[3], p[4],p[5], p[6],p[7]);
+      }
+    }
+  }
+  // Delete
+  nsvgDelete(image);
 */
 
 enum NSVGpaintType {
-	NSVG_PAINT_NONE = 0,
-	NSVG_PAINT_COLOR = 1,
-	NSVG_PAINT_LINEAR_GRADIENT = 2,
-	NSVG_PAINT_RADIAL_GRADIENT = 3,
+  NSVG_PAINT_NONE = 0,
+  NSVG_PAINT_COLOR = 1,
+  NSVG_PAINT_LINEAR_GRADIENT = 2,
+  NSVG_PAINT_RADIAL_GRADIENT = 3,
 };
 
 enum NSVGspreadType {
-	NSVG_SPREAD_PAD = 0,
-	NSVG_SPREAD_REFLECT = 1,
-	NSVG_SPREAD_REPEAT = 2,
+  NSVG_SPREAD_PAD = 0,
+  NSVG_SPREAD_REFLECT = 1,
+  NSVG_SPREAD_REPEAT = 2,
 };
 
 enum NSVGlineJoin {
-	NSVG_JOIN_MITER = 0,
-	NSVG_JOIN_ROUND = 1,
-	NSVG_JOIN_BEVEL = 2,
+  NSVG_JOIN_MITER = 0,
+  NSVG_JOIN_ROUND = 1,
+  NSVG_JOIN_BEVEL = 2,
 };
 
 enum NSVGlineCap {
-	NSVG_CAP_BUTT = 0,
-	NSVG_CAP_ROUND = 1,
-	NSVG_CAP_SQUARE = 2,
+  NSVG_CAP_BUTT = 0,
+  NSVG_CAP_ROUND = 1,
+  NSVG_CAP_SQUARE = 2,
 };
 
 enum NSVGfillRule {
-	NSVG_FILLRULE_NONZERO = 0,
-	NSVG_FILLRULE_EVENODD = 1,
+  NSVG_FILLRULE_NONZERO = 0,
+  NSVG_FILLRULE_EVENODD = 1,
 };
 
 enum NSVGflags {
-	NSVG_FLAGS_VISIBLE = 0x01
+  NSVG_FLAGS_VISIBLE = 0x01
 };
 
 typedef struct NSVGgradientStop {
-	unsigned int color;
-	float offset;
+  unsigned int color;
+  float offset;
 } NSVGgradientStop;
 
 typedef struct NSVGgradient {
-	float xform[6];
-	char spread;
-	float fx, fy;
-	int nstops;
-	NSVGgradientStop stops[1];
+  float xform[6];
+  char spread;
+  float fx, fy;
+  int nstops;
+  NSVGgradientStop stops[1];
 } NSVGgradient;
 
 typedef struct NSVGpaint {
-	/**
-	 * In the original file, using char type (without signed or unsigned) can be interpreted
-	 * as 'unsigned char' in some build environments, like ARM architecture.
-	 * To prevent the unexpected behavior, we replace 'char type' with 'signed char type' here.
-	 */
-	signed char type;
-	union {
-		unsigned int color;
-		NSVGgradient* gradient;
-	};
+  /**
+   * In the original file, using char type (without signed or unsigned) can be interpreted
+   * as 'unsigned char' in some build environments, like ARM architecture.
+   * To prevent the unexpected behavior, we replace 'char type' with 'signed char type' here.
+   */
+  signed char type;
+  union {
+    unsigned int  color;
+    NSVGgradient* gradient;
+  };
 } NSVGpaint;
 
 typedef struct NSVGpath
 {
-	float* pts;					// Cubic bezier points: x0,y0, [cpx1,cpx1,cpx2,cpy2,x1,y1], ...
-	int npts;					// Total number of bezier points.
-	char closed;				// Flag indicating if shapes should be treated as closed.
-	float bounds[4];			// Tight bounding box of the shape [minx,miny,maxx,maxy].
-	struct NSVGpath* next;		// Pointer to next path, or NULL if last element.
+  float*           pts;         // Cubic bezier points: x0,y0, [cpx1,cpx1,cpx2,cpy2,x1,y1], ...
+  int              npts;        // Total number of bezier points.
+  char             closed;      // Flag indicating if shapes should be treated as closed.
+  float            bounds[4];   // Tight bounding box of the shape [minx,miny,maxx,maxy].
+  struct NSVGpath* next;        // Pointer to next path, or NULL if last element.
 } NSVGpath;
 
 typedef struct NSVGshape
 {
-	char id[64];				// Optional 'id' attr of the shape or its group
-	NSVGpaint fill;				// Fill paint
-	NSVGpaint stroke;			// Stroke paint
-	float opacity;				// Opacity of the shape.
-	float strokeWidth;			// Stroke width (scaled).
-	float strokeDashOffset;		// Stroke dash offset (scaled).
-	float strokeDashArray[8];			// Stroke dash array (scaled).
-	char strokeDashCount;				// Number of dash values in dash array.
-	char strokeLineJoin;		// Stroke join type.
-	char strokeLineCap;			// Stroke cap type.
-	char fillRule;				// Fill rule, see NSVGfillRule.
-	unsigned char flags;		// Logical or of NSVG_FLAGS_* flags
-	float bounds[4];			// Tight bounding box of the shape [minx,miny,maxx,maxy].
-	NSVGpath* paths;			// Linked list of paths in the image.
-	struct NSVGshape* next;		// Pointer to next shape, or NULL if last element.
+  char              id[64];     // Optional 'id' attr of the shape or its group
+  NSVGpaint         fill;       // Fill paint
+  NSVGpaint         stroke;     // Stroke paint
+  float             opacity;    // Opacity of the shape.
+  float             strokeWidth; // Stroke width (scaled).
+  float             strokeDashOffset; // Stroke dash offset (scaled).
+  float             strokeDashArray[8]; // Stroke dash array (scaled).
+  char              strokeDashCount; // Number of dash values in dash array.
+  char              strokeLineJoin; // Stroke join type.
+  char              strokeLineCap; // Stroke cap type.
+  float             miterLimit; // Miter limit
+  char              fillRule;   // Fill rule, see NSVGfillRule.
+  unsigned char     flags;      // Logical or of NSVG_FLAGS_* flags
+  float             bounds[4];  // Tight bounding box of the shape [minx,miny,maxx,maxy].
+  NSVGpath*         paths;      // Linked list of paths in the image.
+  struct NSVGshape* next;       // Pointer to next shape, or NULL if last element.
 } NSVGshape;
 
 typedef struct NSVGimage
 {
-	float width;				// Width of the image.
-	float height;				// Height of the image.
-	NSVGshape* shapes;			// Linked list of shapes in the image.
+  float      width;             // Width of the image.
+  float      height;            // Height of the image.
+  NSVGshape* shapes;            // Linked list of shapes in the image.
 } NSVGimage;
 
 // Parses SVG file from a file, returns SVG image as paths.
