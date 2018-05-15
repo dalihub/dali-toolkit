0ef90bd0556554b6f95f81521a1d58aaf5079511
agnelo vaz <agnelo.vaz@samsung.com>
2018-05-15 17:28:08 +0100

    Adding Grid Layout
    Change-Id: I6072e0d3630964d4b561a0b6716642ada715dc1d

---
+++ dali-toolkit/devel-api/layouting/grid.h
@@ -0,0 +1,137 @@
+#ifndef DALI_TOOLKIT_LAYOUTING_GRID_LAYOUT_H
+#define DALI_TOOLKIT_LAYOUTING_GRID_LAYOUT_H
+
+/*
+ * Copyright (c) 2018 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <dali/public-api/common/dali-common.h>
+#include <dali/public-api/object/base-handle.h>
+#include <dali-toolkit/devel-api/layouting/layout-group.h>
+#include <dali-toolkit/devel-api/layouting/layout-size.h>
+#include <dali-toolkit/public-api/toolkit-property-index-ranges.h>
+
+namespace Dali
+{
+namespace Toolkit
+{
+
+namespace Internal DALI_INTERNAL
+{
+class Grid;
+}
+
+/**
+ * This class implements a horizontal box layout, automatically handling
+ * right to left or left to right direction change.
+ */
+class DALI_IMPORT_API Grid : public LayoutGroup
+{
+public:
+
+  enum PropertyRange
+  {
+    CHILD_PROPERTY_START_INDEX = LayoutGroup::CHILD_PROPERTY_START_INDEX+100,
+    CHILD_PROPERTY_END_INDEX   = CHILD_PROPERTY_START_INDEX+1000
+  };
+
+  struct Property
+  {
+    // @todo When we can have event-only properties for BaseObject, this will be useful.
+    enum
+    {
+      CELL_PADDING = PROPERTY_REGISTRATION_START_INDEX + 2000
+    };
+  };
+
+  struct ChildProperty
+  {
+    enum
+    {
+      WEIGHT = CHILD_PROPERTY_START_INDEX
+    };
+  };
+
+  /**
+   * @brief Creates an uninitialized Grid handle.
+   *
+   * Initialize it using Grid::New().
+   * Calling member functions with an uninitialized handle is not allowed.
+   */
+  Grid();
+
+  /**
+   * @brief Creates a Grid object.
+   */
+  static Grid New();
+
+  /**
+   * @brief Downcasts a handle to a Grid handle.
+   *
+   * If handle points to a Grid, the downcast produces a valid handle.
+   * If not, the returned handle is left uninitialized.
+
+   * @param[in] handle to an object
+   * @return Handle to a Grid or an uninitialized handle
+   */
+  static Grid DownCast( BaseHandle handle );
+
+  /**
+   * @brief Copy constructor
+   */
+  Grid( const Grid& other );
+
+  /**
+   * @brief Assigment operator
+   */
+  Grid& operator=( const Grid& other );
+
+  /**
+   * @brief Default destructor.
+   *
+   * This is non-virtual, since derived Handle types must not contain data or virtual methods
+   */
+  ~Grid()=default;
+
+  /**
+   * @brief Set the padding between cells in the layout
+   *
+   * @param[in] size The padding between cells.
+   */
+  void SetCellPadding( LayoutSize size );
+
+  /**
+   * @brief Get the padding between cells in the layout
+   *
+   * @return The padding between cells.
+   */
+  LayoutSize GetCellPadding();
+
+public: // Not intended for application developers
+
+  /// @cond internal
+  /**
+   * @brief This constructor is used by Grid::New() methods.
+   *
+   * @param[in] actor A pointer to a newly allocated Dali resource
+   */
+  explicit DALI_INTERNAL Grid( Internal::Grid* body );
+  /// @endcond
+};
+
+} // namespace Toolkit
+} // namespace Dali
+
+#endif // DALI_TOOLKIT_LAYOUTING_GRID_LAYOUT_H
---
+++ dali-toolkit/internal/layouting/grid-impl.cpp
@@ -0,0 +1,331 @@
+/*
+ * Copyright (c) 2018 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+//CLASS HEADER
+#include <dali-toolkit/internal/layouting/grid-impl.h>
+
+//EXTERNAL HEADERS
+#include <dali/integration-api/debug.h>
+#include <dali/public-api/common/extents.h>
+#include <dali/public-api/actors/actor.h>
+
+//INTERNAL HEADERS
+#include <dali-toolkit/devel-api/layouting/layout-item.h>
+#include <dali-toolkit/public-api/controls/control-impl.h>
+#include <dali-toolkit/internal/controls/control/control-data-impl.h>
+#include <dali-toolkit/devel-api/layouting/layout-length.h>
+
+
+#if defined(DEBUG_ENABLED)
+static Debug::Filter* gLogFilter = Debug::Filter::New( Debug::Concise, false, "LOG_LAYOUT" );
+#endif
+
+namespace Dali
+{
+namespace Toolkit
+{
+namespace Internal
+{
+
+GridPtr Grid::New()
+{
+  GridPtr layout( new Grid() );
+  return layout;
+}
+
+Grid::Grid()
+: LayoutGroup(),
+  mCellPadding( 0, 0 ),
+  mTotalLength( 0 )
+{
+}
+
+Grid::~Grid()
+{
+}
+
+void Grid::DoInitialize()
+{
+}
+
+void Grid::DoRegisterChildProperties( const std::string& containerType )
+{
+  auto typeInfo = Dali::TypeRegistry::Get().GetTypeInfo( containerType );
+  if( typeInfo )
+  {
+    Property::IndexContainer indices;
+    typeInfo.GetChildPropertyIndices( indices );
+
+    if( std::find( indices.Begin(), indices.End(), Toolkit::Grid::ChildProperty::WEIGHT ) == indices.End() )
+    {
+      ChildPropertyRegistration( typeInfo.GetName(), "weight",
+                                 Toolkit::Grid::ChildProperty::WEIGHT, Property::FLOAT );
+    }
+  }
+}
+
+void Grid::OnChildAdd( LayoutItem& child )
+{
+  auto owner = child.GetOwner();
+  owner.SetProperty( Toolkit::Grid::ChildProperty::WEIGHT, 1.0f );
+}
+
+void Grid::SetCellPadding( LayoutSize size )
+{
+  mCellPadding = size;
+}
+
+LayoutSize Grid::GetCellPadding()
+{
+  return mCellPadding;
+}
+
+void Grid::OnMeasure( MeasureSpec widthMeasureSpec, MeasureSpec heightMeasureSpec )
+{
+#if defined(DEBUG_ENABLED)
+  auto actor = Actor::DownCast(GetOwner());
+
+  std::ostringstream oss;
+  oss << "Grid::OnMeasure  ";
+  if( actor )
+  {
+    oss << "Actor Id:" << actor.GetId() << " Name:" << actor.GetName() << "  ";
+  }
+  oss << "widthMeasureSpec:" << widthMeasureSpec << " heightMeasureSpec:" << heightMeasureSpec << std::endl;
+  DALI_LOG_INFO( gLogFilter, Debug::Concise, oss.str().c_str() );
+#endif
+
+  DALI_LOG_INFO( gLogFilter, Debug::Concise, "Grid::OnMeasure widthSize(%d) \n", widthMeasureSpec.GetSize());
+
+  auto widthMode = widthMeasureSpec.GetMode();
+  auto heightMode = heightMeasureSpec.GetMode();
+  LayoutLength widthSize = widthMeasureSpec.GetSize();
+  LayoutLength heightSize = heightMeasureSpec.GetSize();
+
+  auto padding = Extents(); // todo , should be the padding of this control or just the column padding. Column most likely.
+
+  if( widthMode == MeasureSpec::Mode::UNSPECIFIED )
+  {
+    if( mColumnWidth > 0 ) //explict value assigned
+    {
+      widthSize = mColumnWidth + padding.start + padding.end;
+    }
+    else
+    {
+      widthSize = padding.start + padding.end;
+    }
+  }
+
+  // used to determine colums
+  //int childWidth = widthSize - padding.start - padding.end;
+
+//  bool isExactly = (widthMode == MeasureSpec::Mode::EXACTLY);
+//  bool matchHeight = false;
+//  bool allFillParent = true;
+//  LayoutLength maxHeight = 0;
+//  LayoutLength alternativeMaxHeight = 0;
+
+  struct
+  {
+    MeasuredSize::State widthState;
+    MeasuredSize::State heightState;
+  } childState = { MeasuredSize::State::MEASURED_SIZE_OK, MeasuredSize::State::MEASURED_SIZE_OK };
+
+  // measure children, and determine if further resolution is required
+  auto count = GetChildCount();
+  for( auto i=0u; i<count; ++i )
+  {
+    auto childLayoutItem = GetChildAt( i );
+    if( childLayoutItem )
+    {
+      auto childOwner = childLayoutItem->GetOwner();
+      //auto desiredHeight = childOwner.GetProperty<int>( Toolkit::LayoutItem::ChildProperty::HEIGHT_SPECIFICATION );
+
+      MeasureChild( childLayoutItem, widthMeasureSpec, heightMeasureSpec );
+      auto childHeight = childLayoutItem->GetMeasuredHeight();
+      //auto childMargin = Extents(0,0,0,0);//childLayoutItem->GetMargin();
+
+     // childState = combineMeasuredStates(childState, child.getMeasuredState());
+
+      if ( heightMode == MeasureSpec::Mode::UNSPECIFIED)
+      {
+         heightSize = padding.top + padding.bottom + childHeight /*+ getVerticalFadingEdgeLength() * 2*/;
+      }
+
+      if (heightMode == MeasureSpec::Mode::AT_MOST)
+      {
+          int ourSize =  padding.top + padding.bottom;
+
+          for( auto i = 0u; i < count; i += mNumColumns)
+          {
+            ourSize += childHeight;
+            if(i + mNumColumns < count )
+            {
+              ourSize += mVerticalSpacing;
+            }
+            if (ourSize >= heightSize)
+            {
+              ourSize = heightSize;
+              break;
+            }
+          }
+          heightSize = ourSize;
+      }
+///
+
+      if(widthMode == MeasureSpec::Mode::AT_MOST && mRequestedNumColumns != AUTO_FIT)
+      {
+          int ourSize = (mRequestedNumColumns*mColumnWidth)
+                        + ((mRequestedNumColumns-1)*mHorizontalSpacing)
+                        + padding.start + padding.end;
+
+          if( ourSize > widthSize /*|| didNotInitiallyFit */)
+          {
+            // widthSize |= MeasuredSize::State::MEASURED_SIZE_TOO_SMALL;  OR
+            //   MeasuredSize widthSizeAndState = ResolveSizeAndState( widthSize, widthMeasureSpec, MeasuredSize::State::MEASURED_SIZE_OK);
+
+          }
+      }
+
+      SetMeasuredDimensions( ResolveSizeAndState( widthSize, widthMeasureSpec, childState.widthState ),
+                             ResolveSizeAndState( heightSize, heightMeasureSpec, childState.heightState ) );
+
+    }
+  } // Iterating through children
+
+  Extents layoutPadding = GetPadding();
+  mTotalLength += layoutPadding.start + layoutPadding.end;
+  widthSize = std::max( mTotalLength, GetSuggestedMinimumWidth() );
+  MeasuredSize widthSizeAndState = ResolveSizeAndState( widthSize, widthMeasureSpec, MeasuredSize::State::MEASURED_SIZE_OK);
+  widthSize = widthSizeAndState.GetSize();
+
+  //if( !allFillParent && heightMode != MeasureSpec::Mode::EXACTLY )
+  {
+    //heightSize = alternativeMaxHeight;
+  }
+
+  heightSize += layoutPadding.top + layoutPadding.bottom;
+  //heightSize = std::max( maxHeight, GetSuggestedMinimumHeight() );
+
+  widthSizeAndState.SetState( childState.widthState );
+
+  SetMeasuredDimensions( widthSizeAndState,
+                         ResolveSizeAndState( heightSize, heightMeasureSpec, childState.heightState ) );
+
+  //if( matchHeight )
+  //{
+   // ForceUniformHeight( GetChildCount(), widthMeasureSpec );
+ // }
+}
+
+void Grid::ForceUniformHeight( int count, MeasureSpec widthMeasureSpec )
+{
+  // Pretend that the linear layout has an exact size. This is the measured height of
+  // ourselves. The measured height should be the max height of the children, changed
+  // to accommodate the heightMeasureSpec from the parent
+  auto uniformMeasureSpec = MeasureSpec( GetMeasuredHeight(), MeasureSpec::Mode::EXACTLY );
+  for (int i = 0; i < count; ++i)
+  {
+    LayoutItemPtr childLayout = GetChildAt(i);
+    if( childLayout != nullptr )
+    {
+      auto childOwner = childLayout->GetOwner();
+      auto desiredWidth = childOwner.GetProperty<int>( Toolkit::LayoutItem::ChildProperty::WIDTH_SPECIFICATION );
+      auto desiredHeight = childOwner.GetProperty<int>( Toolkit::LayoutItem::ChildProperty::HEIGHT_SPECIFICATION );
+
+      if( desiredHeight == Toolkit::ChildLayoutData::MATCH_PARENT )
+      {
+        // Temporarily force children to reuse their old measured width
+        int oldWidth = desiredWidth;
+        childOwner.SetProperty( Toolkit::LayoutItem::ChildProperty::WIDTH_SPECIFICATION, childLayout->GetMeasuredWidth().mValue );
+
+        // Remeasure with new dimensions
+        MeasureChildWithMargins( childLayout, widthMeasureSpec, 0, uniformMeasureSpec, 0);
+
+        childOwner.SetProperty( Toolkit::LayoutItem::ChildProperty::WIDTH_SPECIFICATION, oldWidth );
+      }
+    }
+  }
+}
+
+void Grid::OnLayout( bool changed, LayoutLength left, LayoutLength top, LayoutLength right, LayoutLength bottom )
+{
+  auto owner = GetOwner();
+  auto actor = Actor::DownCast(owner);
+  bool isLayoutRtl = actor ? actor.GetProperty<bool>( Actor::Property::LAYOUT_DIRECTION ) : false;
+
+  Extents padding = GetPadding();
+
+  auto count = GetChildCount();
+
+  int start = 0;
+  int dir = 1;
+
+  // In case of RTL, start drawing from the last child.
+  // @todo re-work to draw the first child from the right edge, and move leftwards.
+  // (Should have an alignment also)
+  if( isLayoutRtl ) {
+    start = count - 1;
+    dir = -1;
+  }
+
+  for( unsigned int i = 0; i < count; i++)
+  {
+    // for each child
+    int childIndex = start + dir * i;
+    LayoutItemPtr childLayout = GetChildAt( childIndex );
+    if( childLayout != nullptr )
+    {
+      // Get column and row spec
+  
+
+      // Get start and end position of child x1,x2
+      auto x1 = 0;
+      auto x2 = 1;
+      // Get top and bottom position of child y1,y2
+      auto y1 = 0;
+      auto y2 = 1;
+      // Get cell width and height
+      auto cellWidth = x2 - x1;
+      auto cellHeight = y2 - y1;
+
+      // Get child measured size
+      auto childWidth = childLayout->GetMeasuredWidth();
+      auto childHeight = childLayout->GetMeasuredHeight();
+
+      auto childMargin = Extents(0,0,0,0); //childLayout->GetMargin();
+
+      int sumMarginsX = childMargin.start + childMargin.end;
+      int sumMarginsY = childMargin.top + childMargin.bottom;
+
+      cellWidth -= sumMarginsX;
+      cellHeight -= sumMarginsY;
+
+      // Get szie in of child in the cell
+      //int width = hAlign.getSizeInCell(c, childWidth, cellWidth - sumMarginsX);
+      //int height = vAlign.getSizeInCell(c, childHeight, cellHeight - sumMarginsY);
+
+      auto childTop = LayoutLength(padding.top) + childMargin.top;
+      auto childLeft = childMargin.start;
+
+      childLayout->Layout( childLeft, childTop, childLeft + childWidth, childTop + childHeight );
+    }
+  }
+}
+
+} // namespace Internal
+} // namespace Toolkit
+} // namespace Dali
