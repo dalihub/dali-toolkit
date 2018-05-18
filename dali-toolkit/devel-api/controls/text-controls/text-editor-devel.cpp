fa86d00f5d97d5d7aaf936f83e217c730347c766
Heeyong Song <heeyong.song@samsung.com>
2018-05-18 10:04:37 +0900

    Revert "[Tizen] Fix can't call GetInputMethodContext api issue"
    This reverts commit bcafa3b79c5344d96b560a3f33177db64e75bb21.
    
    Change-Id: I175e6d3d44cb62cb56dd6bbd231e9e65a4d74012

--- dali-toolkit/devel-api/controls/text-controls/text-editor-devel.cpp
+++ dali-toolkit/devel-api/controls/text-controls/text-editor-devel.cpp
@@ -1,5 +1,8 @@
+#ifndef DALI_TOOLKIT_TEXT_EDITOR_DEVEL_H
+#define DALI_TOOLKIT_TEXT_EDITOR_DEVEL_H
+
 /*
- * Copyright (c) 2018 Samsung Electronics Co., Ltd.
+ * Copyright (c) 2017 Samsung Electronics Co., Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -38,3 +41,5 @@ InputMethodContext GetInputMethodContext( TextEditor textEditor )
 } // namespace Toolkit
 
 } // namespace Dali
+
+#endif // DALI_TOOLKIT_TEXT_EDITOR_DEVEL_H
