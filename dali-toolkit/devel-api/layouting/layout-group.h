6ae5497d0b0d6f94d43261b2d527a4f590ee6fe2
Wonsik Jung <sidein@samsung.com>
2018-06-11 15:30:25 +0900

    [Tizen] Change the API macros for layout
    This reverts commit f57e8f63d7def520df0580f64d664c816f1c4803.
    
    Change-Id: I41f8fbf2831645f974540e534160a6bc9c746fc4

--- dali-toolkit/devel-api/layouting/layout-group.h
+++ dali-toolkit/devel-api/layouting/layout-group.h
@@ -22,6 +22,7 @@
 #include <dali-toolkit/devel-api/toolkit-property-index-ranges.h>
 #include <dali-toolkit/devel-api/layouting/layout-item.h>
 #include <dali-toolkit/devel-api/layouting/measure-spec.h>
+#include <dali-toolkit/public-api/dali-toolkit-common.h>
 
 namespace Dali
 {
@@ -49,7 +50,7 @@ class LayoutGroup;
  * To write a new layout, inherit from both LayoutGroup handle and Internal::LayoutGroup body.
  *
  */
-class DALI_IMPORT_API LayoutGroup : public LayoutItem
+class DALI_TOOLKIT_API LayoutGroup : public LayoutItem
 {
 public:
   using LayoutId = unsigned int;
