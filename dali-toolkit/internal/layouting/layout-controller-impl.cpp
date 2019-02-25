df9f57f40038391a7d1e7c8c531def42cb2ff690
Sunghyun kim <scholb.kim@samsung.com>
2019-02-25 15:57:55 +0900

    Revert "[Tizen] Fix z value issue"
    This reverts commit 70473dbe82e68393b85e8ecae54e64135c495c55.
    
    Change-Id: I090a65759dc1ec980ceb31e1bbb713e7ab6f98e7

--- dali-toolkit/internal/layouting/layout-controller-impl.cpp
+++ dali-toolkit/internal/layouting/layout-controller-impl.cpp
@@ -347,19 +347,13 @@ void LayoutController::PerformLayoutPositioning( LayoutPositionDataArray& layout
     {
       if( !layoutPositionData.animated )
       {
-        actor.SetX( layoutPositionData.left );
-        actor.SetY( layoutPositionData.top );
-
-        actor.SetProperty( Actor::Property::SIZE_WIDTH, layoutPositionData.right - layoutPositionData.left );
-        actor.SetProperty( Actor::Property::SIZE_HEIGHT, layoutPositionData.bottom - layoutPositionData.top );
+        actor.SetPosition( layoutPositionData.left, layoutPositionData.top );
+        actor.SetSize( layoutPositionData.right - layoutPositionData.left, layoutPositionData.bottom - layoutPositionData.top );
       }
       else
       {
-        actor.SetX( actor.GetCurrentPosition().x );
-        actor.SetY( actor.GetCurrentPosition().y );
-
-        actor.SetProperty( Actor::Property::SIZE_WIDTH, actor.GetCurrentSize().x );
-        actor.SetProperty( Actor::Property::SIZE_HEIGHT, actor.GetCurrentSize().y );
+        actor.SetPosition( actor.GetCurrentPosition() );
+        actor.SetSize( actor.GetCurrentSize() );
       }
     }
   }
