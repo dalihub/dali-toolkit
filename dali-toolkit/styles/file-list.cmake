647cfb67dc222a7577ca461f2baa7e91359b26e2
adam.b <adam.b@samsung.com>
2019-08-29 19:14:08 +0100

    CMake build scripts
    Change-Id: Ife81f050d3e31022484d2fb9180114fa07097343

---
+++ dali-toolkit/styles/file-list.cmake
@@ -0,0 +1,13 @@
+# This file is auto-generated!
+#
+# Files to install here
+SET( dali_toolkit_style_files
+    ${toolkit_styles_base_dir}/*.json
+    ${toolkit_styles_dir}/*.json
+)
+
+
+SET( dali_toolkit_style_images
+    ${toolkit_style_images_dir}/*.png
+)
+
