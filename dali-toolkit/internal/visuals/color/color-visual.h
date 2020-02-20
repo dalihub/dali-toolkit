59e01304b26f89e7acd2d3dd77d3fe0902c520d9
Heeyong Song <heeyong.song@samsung.com>
2020-02-20 10:19:31 +0900

    Support the blur radius of the ColorVisual
    Change-Id: Iaee00d74e327372c040b5137e0ce1666b5a0b59b

--- dali-toolkit/internal/visuals/color/color-visual.h
+++ dali-toolkit/internal/visuals/color/color-visual.h
@@ -2,7 +2,7 @@
 #define DALI_TOOLKIT_INTERNAL_COLOR_VISUAL_H
 
 /*
- * Copyright (c) 2019 Samsung Electronics Co., Ltd.
+ * Copyright (c) 2020 Samsung Electronics Co., Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -115,6 +115,7 @@ private:
 
 private:
 
+  float mBlurRadius;         ///< The blur radius
   bool mRenderIfTransparent; ///< Whether we should render even if the mix-color is transparent.
 };
 
