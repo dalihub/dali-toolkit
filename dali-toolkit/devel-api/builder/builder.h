ce16950a8cd44940cc18ec51919f99afbde45f2d
Adeel Kazmi <adeel.kazmi@samsung.com>
2020-06-17 12:06:57 +0100

    FrameBuffer Usage Removal
    Change-Id: Ib709a407334caf049a02d5ccd2f8a3e42a476c96

--- dali-toolkit/devel-api/builder/builder.h
+++ dali-toolkit/devel-api/builder/builder.h
@@ -2,7 +2,7 @@
 #define DALI_TOOLKIT_UIBUILDER_H
 
 /*
- * Copyright (c) 2019 Samsung Electronics Co., Ltd.
+ * Copyright (c) 2020 Samsung Electronics Co., Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,7 +23,6 @@
 #include <dali/public-api/animation/animation.h>
 #include <dali/public-api/animation/linear-constrainer.h>
 #include <dali/devel-api/animation/path-constrainer.h>
-#include <dali/public-api/images/frame-buffer-image.h>
 
 // INTERNAL INCLUDES
 #include <dali-toolkit/public-api/dali-toolkit-common.h>
