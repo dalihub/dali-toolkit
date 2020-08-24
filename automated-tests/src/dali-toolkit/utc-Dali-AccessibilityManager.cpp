bc1eca01b79d8a39bcee5517b65a77814d4847de
Joogab Yun <joogab.yun@samsung.com>
2020-08-24 14:28:35 +0900

    Revert "Revert "Removed TouchEvent from dali-core""
    This reverts commit ba604915f436f3cf5451fc810f992dbc229f8ba8.

--- automated-tests/src/dali-toolkit/utc-Dali-AccessibilityManager.cpp
+++ automated-tests/src/dali-toolkit/utc-Dali-AccessibilityManager.cpp
@@ -2218,10 +2218,11 @@ public:
   {
   }
 
-  bool Callback( AccessibilityManager& accessibilityManager, const Dali::TouchEvent& touchEvent )
+  bool Callback( AccessibilityManager& accessibilityManager, const Dali::TouchData& touchEvent )
   {
     mCalls++;
-    mTouchEvent = touchEvent;
+    Dali::TouchData handle(touchEvent); // Ensure it's ref-counted
+    mTouchData = handle;
     tet_infoline( "Signal called" );
     return true;
   }
@@ -2231,14 +2232,14 @@ public:
     return mCalls;
   }
 
-  const Dali::TouchEvent& GetTouchEvent() const
+  const Dali::TouchData& GetTouchData() const
   {
-    return mTouchEvent;
+    return mTouchData;
   }
 
 private:
   unsigned int mCalls;         ///< Keeps track of how many times the signal has been called.
-  Dali::TouchEvent mTouchEvent; ///< Stores the last touch event received.
+  Dali::TouchData mTouchData ; ///< Stores the last touch event received.
 };
 
 int UtcDaliAccessibilityManagerActionScrollSignalP(void)
@@ -2260,14 +2261,13 @@ int UtcDaliAccessibilityManagerActionScrollSignalP(void)
 
   DALI_TEST_EQUALS( callback.GetCalls(), 1u, TEST_LOCATION );
 
-  const TouchEvent& signalTouchEvent = callback.GetTouchEvent();
-  DALI_TEST_EQUALS( signalTouchEvent.GetPointCount(), 1u, TEST_LOCATION );
+  const TouchData& signalTouchData = callback.GetTouchData();
+  DALI_TEST_EQUALS( signalTouchData.GetPointCount(), 1u, TEST_LOCATION );
 
-  const TouchPoint& signalTouchPoint = signalTouchEvent.GetPoint( 0u );
 
-  DALI_TEST_EQUALS( signalTouchPoint.state, TouchPoint::Started, TEST_LOCATION );
-  DALI_TEST_EQUALS( signalTouchPoint.screen.x, 100.0f, TEST_LOCATION );
-  DALI_TEST_EQUALS( signalTouchPoint.screen.y, 200.0f, TEST_LOCATION );
+  DALI_TEST_EQUALS( signalTouchData.GetState(0), PointState::STARTED, TEST_LOCATION );
+  DALI_TEST_EQUALS( signalTouchData.GetScreenPosition(0).x, 100.0f, TEST_LOCATION );
+  DALI_TEST_EQUALS( signalTouchData.GetScreenPosition(0).y, 200.0f, TEST_LOCATION );
 
   END_TEST;
 }
