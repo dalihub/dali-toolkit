60d99d04d7677a1b91d4f2fdd9e74bc5e1b5e7bf
ali <ali198724@gmail.com>
2020-08-31 15:41:01 +0300

    Text selection refactoring
    This Task contains two main parts:
    
    1- Introduce internal interface Dali::Toolkit::Text::SelectableControlInterface which is inherited by TextEditor and TextField
    Both Class needs to implement this interface methods, else developer will get compilation error
    
    2- Introduce Properties to get/set Text Selection Ranges start and end positions in Devel namespace, SELECTED_TEXT_START (start position) SELECTED_TEXT_END (end position)
    this allow programatical set for text selection, also get selection range programatically
    
    Change-Id: I7ec96d4f1570218f0e945f21a03a6a10e5e8fe97

---
+++ dali-toolkit/internal/text/text-selectable-control-interface.h
@@ -0,0 +1,63 @@
+#ifndef DALI_TOOLKIT_TEXT_SELECTABLE_CONTROL_INTERFACE_H
+#define DALI_TOOLKIT_TEXT_SELECTABLE_CONTROL_INTERFACE_H
+
+/*
+ * Copyright (c) 2020 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+namespace Dali
+{
+
+namespace Toolkit
+{
+
+using  Uint32Pair = std::pair<uint32_t,uint32_t>;
+namespace Text
+{
+
+/**
+ * @brief An interface that the Text::Controller used for text selection functionality.
+ */
+class SelectableControlInterface
+{
+public:
+
+  /**
+   * @brief Virtual destructor.
+   */
+  virtual ~SelectableControlInterface() = default;
+
+  /**
+   * @brief Called to set the selection postions in the texts.
+   * @param start start selection position (pass NULL to ignore).
+   * @param end end selection position (pass NULL to ignore).
+   */
+  virtual void SetTextSelectionRange(const uint32_t *start, const uint32_t *end) = 0;
+
+  /**
+   * @brief Called to return the selection postions in the texts.
+   * @return pair contains start and end positions.
+   */
+  virtual Uint32Pair GetTextSelectionRange() const = 0;
+};
+
+} // namespace Text
+
+} // namespace Toolkit
+
+} // namespace Dali
+
+#endif // DALI_TOOLKIT_TEXT_SELECTABLE_CONTROL_INTERFACE_H
