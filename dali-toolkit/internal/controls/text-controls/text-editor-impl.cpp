e0ca203b93c8af4e14f69e2c678d890d4533f306
Sunghyun Kim <scholb.kim@samsung.com>
2020-09-23 11:21:38 +0900

    Revert "[Tizen] Revert "Changes after touch consumed behaviour change""
    This reverts commit 2d1bfc90245bb9327656144df94441d947502d16.

--- dali-toolkit/internal/controls/text-controls/text-editor-impl.cpp
+++ dali-toolkit/internal/controls/text-controls/text-editor-impl.cpp
@@ -1865,7 +1865,7 @@ void TextEditor::OnSceneConnection( int depth )
 
 bool TextEditor::OnTouched( Actor actor, const TouchEvent& touch )
 {
-  return true;
+  return false;
 }
 
 void TextEditor::OnIdleSignal()
