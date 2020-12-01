761f86ced052ce7a6e9632828498cf40fee47402
Joogab Yun <joogab.yun@samsung.com>
2020-12-01 13:55:56 +0900

    [Tizen] Add AutofillContainer class and autofill implementation
    This reverts commit 7cf6a992018691a8262db5494bd593dc4df3a98e.
    
    Change-Id: Iaef7479aab8aad78d3418914ca77dacfc62f3104

--- dali-toolkit/internal/controls/control/control-data-impl.cpp
+++ dali-toolkit/internal/controls/control/control-data-impl.cpp
@@ -433,11 +433,14 @@ Control::Impl::Impl( Control& controlImpl )
   mTooltip( NULL ),
   mInputMethodContext(),
   mIdleCallback(nullptr),
+  mAutofillItem(),
+  mAutofillContainer(),
   mFlags( Control::ControlBehaviour( CONTROL_BEHAVIOUR_DEFAULT ) ),
   mIsKeyboardNavigationSupported( false ),
   mIsKeyboardFocusGroup( false ),
   mIsEmittingResourceReadySignal(false),
-  mNeedToEmitResourceReady(false)
+  mNeedToEmitResourceReady(false),
+  mIsAutofillEnabled( false )
 {
   Dali::Accessibility::Accessible::RegisterControlAccessibilityGetter(
       []( Dali::Actor actor ) -> Dali::Accessibility::Accessible* {
@@ -2319,6 +2322,36 @@ void Control::Impl::AccessibilityDeregister()
     accessibilityNotificationCulled = {};
     accessibilityNotificationSet = false;
   }
+}
+
+void Control::Impl::SetAutofillEnabled( bool autofillEnabled )
+{
+  mIsAutofillEnabled = autofillEnabled;
+}
+
+bool Control::Impl::IsAutofillEnabled()
+{
+  return mIsAutofillEnabled;
+}
+
+void Control::Impl::SetAutofillItemHandle( Dali::AutofillItem item )
+{
+  mAutofillItem = item;
+}
+
+Dali::AutofillItem Control::Impl::GetAutofillItemHandle()
+{
+  return mAutofillItem;
+}
+
+void Control::Impl::SetAutofillContainer( Toolkit::AutofillContainer container )
+{
+  mAutofillContainer = container;
+}
+
+Toolkit::AutofillContainer Control::Impl::GetAutofillContainer()
+{
+  return mAutofillContainer;
 }
 
 } // namespace Internal
