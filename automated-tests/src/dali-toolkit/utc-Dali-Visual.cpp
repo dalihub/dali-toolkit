a05b8357fcb13f3ff935c936fbb055b51ee18f17
Eunki, Hong <eunkiki.hong@samsung.com>
2021-12-31 15:55:08 +0900

    Make DoCreatePropertyMap use index key
    Only image-visual's SynchronousLoading used string key.
    It is related with TizenFX's implementation.
    
    It is outlier. So we should change both of them
    
    Change-Id: Iedd002fb24c945cc1fdac94c38ec04198536b4a2
    Signed-off-by: Eunki, Hong <eunkiki.hong@samsung.com>

--- automated-tests/src/dali-toolkit/utc-Dali-Visual.cpp
+++ automated-tests/src/dali-toolkit/utc-Dali-Visual.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Samsung Electronics Co., Ltd.
+ * Copyright (c) 2022 Samsung Electronics Co., Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -890,7 +890,7 @@ int UtcDaliVisualGetPropertyMap5(void)
   DALI_TEST_CHECK( value );
   DALI_TEST_CHECK(  value->Get<int>() == WrapMode::MIRRORED_REPEAT);
 
-  value = resultMap.Find( "synchronousLoading",   Property::BOOLEAN );
+  value = resultMap.Find( ImageVisual::Property::SYNCHRONOUS_LOADING, Property::BOOLEAN );
   DALI_TEST_CHECK( value );
   DALI_TEST_CHECK( value->Get<bool>() == true );
 
