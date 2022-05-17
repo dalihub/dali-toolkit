7875069ceecfd24c2a8cf3a28564f7d115296e93
Richard <r.huang@samsung.com>
2022-05-17 13:47:00 +0100

    Facial animation support
    Change-Id: I079348f3164b53704f7a7c6a652e22afc7195d0b

---
+++ automated-tests/src/dali-scene-loader/utc-Dali-FacialAnimation.cpp
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) 2022 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include <dali-test-suite-utils.h>
+#include <dali-scene-loader/public-api/animation-definition.h>
+#include <dali-scene-loader/public-api/facial-animation-loader.h>
+
+using namespace Dali;
+using namespace Dali::SceneLoader;
+
+int UtcDaliLoadFacialAnimation(void)
+{
+  TestApplication app;
+
+  AnimationDefinition animDef = LoadFacialAnimation(TEST_RESOURCE_DIR "/facial-blendshape-animation.json");
+
+  DALI_TEST_EQUAL(animDef.mName, "Facial_Blendshape_Animation");
+  DALI_TEST_EQUAL(animDef.mDuration, 14.966001f);
+  DALI_TEST_EQUAL(animDef.mEndAction, Animation::BAKE);
+  DALI_TEST_EQUAL(animDef.mSpeedFactor, 1.0f);
+  DALI_TEST_EQUAL(animDef.mLoopCount, 1);
+  DALI_TEST_EQUAL(animDef.mProperties.size(), 122);
+
+  DALI_TEST_EQUAL(animDef.mProperties[0].mNodeName, "GEO_1");
+  DALI_TEST_EQUAL(animDef.mProperties[0].mPropertyName, "uBlendShapeWeight[0]");
+  DALI_TEST_EQUAL(animDef.mProperties[0].mKeyFrames.GetType(), Property::Type::FLOAT);
+  DALI_TEST_EQUAL(animDef.mProperties[0].mTimePeriod.delaySeconds, 0.0f);
+  DALI_TEST_EQUAL(animDef.mProperties[0].mTimePeriod.durationSeconds, 14.966001f);
+
+  DALI_TEST_EQUAL(animDef.mProperties[69].mNodeName, "GEO_2");
+  DALI_TEST_EQUAL(animDef.mProperties[69].mPropertyName, "uBlendShapeWeight[1]");
+  DALI_TEST_EQUAL(animDef.mProperties[69].mKeyFrames.GetType(), Property::Type::FLOAT);
+  DALI_TEST_EQUAL(animDef.mProperties[69].mTimePeriod.delaySeconds, 0.0f);
+  DALI_TEST_EQUAL(animDef.mProperties[69].mTimePeriod.durationSeconds, 14.966001f);
+
+  DALI_TEST_EQUAL(animDef.mProperties[86].mNodeName, "GEO_3");
+  DALI_TEST_EQUAL(animDef.mProperties[86].mPropertyName, "uBlendShapeWeight[2]");
+  DALI_TEST_EQUAL(animDef.mProperties[86].mKeyFrames.GetType(), Property::Type::FLOAT);
+  DALI_TEST_EQUAL(animDef.mProperties[86].mTimePeriod.delaySeconds, 0.0f);
+  DALI_TEST_EQUAL(animDef.mProperties[86].mTimePeriod.durationSeconds, 14.966001f);
+
+  DALI_TEST_EQUAL(animDef.mProperties[100].mNodeName, "GEO_4");
+  DALI_TEST_EQUAL(animDef.mProperties[100].mPropertyName, "uBlendShapeWeight[7]");
+  DALI_TEST_EQUAL(animDef.mProperties[100].mKeyFrames.GetType(), Property::Type::FLOAT);
+  DALI_TEST_EQUAL(animDef.mProperties[100].mTimePeriod.delaySeconds, 0.0f);
+  DALI_TEST_EQUAL(animDef.mProperties[100].mTimePeriod.durationSeconds, 14.966001f);
+
+  DALI_TEST_EQUAL(animDef.mProperties[121].mNodeName, "GEO_5");
+  DALI_TEST_EQUAL(animDef.mProperties[121].mPropertyName, "uBlendShapeWeight[19]");
+  DALI_TEST_EQUAL(animDef.mProperties[121].mKeyFrames.GetType(), Property::Type::FLOAT);
+  DALI_TEST_EQUAL(animDef.mProperties[121].mTimePeriod.delaySeconds, 0.0f);
+  DALI_TEST_EQUAL(animDef.mProperties[121].mTimePeriod.durationSeconds, 14.966001f);
+
+  auto actor = Actor::New();
+  actor.SetProperty(Actor::Property::NAME, "GEO_1");
+
+  char        weightNameBuffer[32];
+  char* const pWeightName = weightNameBuffer + snprintf(weightNameBuffer, sizeof(weightNameBuffer), "%s", "uBlendShapeWeight");
+  for(int i = 0; i < 122; i++)
+  {
+    snprintf(pWeightName, sizeof(weightNameBuffer) - (pWeightName - weightNameBuffer), "[%d]", i);
+    std::string weightName{weightNameBuffer};
+    actor.RegisterProperty(weightName, 0.0f);
+  }
+
+  auto getActor = [&actor](const std::string& name) {
+    return actor.FindChildByName(name);
+  };
+
+  auto anim = animDef.ReAnimate(getActor);
+  DALI_TEST_EQUAL(anim.GetDuration(), animDef.mDuration);
+  DALI_TEST_EQUAL(anim.GetEndAction(), animDef.mEndAction);
+  DALI_TEST_EQUAL(anim.GetSpeedFactor(), animDef.mSpeedFactor);
+  DALI_TEST_EQUAL(anim.GetLoopCount(), animDef.mLoopCount);
+
+  END_TEST;
+}
---
+++ dali-scene-loader/public-api/facial-animation-loader.cpp
@@ -0,0 +1,177 @@
+/*
+ * Copyright (c) 2022 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+// FILE HEADER
+#include <dali-scene-loader/public-api/facial-animation-loader.h>
+
+// INTERNAL INCLUDES
+#include <dali-scene-loader/internal/json-reader.h>
+#include <dali-scene-loader/public-api/blend-shape-details.h>
+#include <dali-scene-loader/public-api/utils.h>
+
+namespace js = json;
+
+namespace Dali
+{
+namespace
+{
+const float MILLISECONDS_TO_SECONDS = 0.001f;
+
+struct BlendShape
+{
+  std::vector<std::vector<float>> mKeys;
+  std::string_view                mNodeName;
+  uint32_t                        mNumberOfMorphTarget;
+  std::string_view                mVersion;
+  std::string_view                mFullName;
+  std::vector<std::string_view>   mMorphNames;
+};
+
+struct FacialAnimation
+{
+  std::string_view        mName;
+  std::vector<BlendShape> mBlendShapes;
+  std::string_view        mVersion;
+  uint32_t                mNumberOfShapes;
+  std::vector<uint32_t>   mTime;
+  uint32_t                mNumberOfFrames;
+};
+
+std::vector<std::vector<float>> ReadBlendShapeKeys(const json_value_s& j)
+{
+  auto&                           jo = js::Cast<json_array_s>(j);
+  std::vector<std::vector<float>> result;
+
+  result.reserve(jo.length);
+
+  auto i = jo.start;
+  while(i)
+  {
+    result.push_back(std::move(js::Read::Array<float, js::Read::Number>(*i->value)));
+    i = i->next;
+  }
+
+  return result;
+}
+
+const auto BLEND_SHAPE_READER = std::move(js::Reader<BlendShape>()
+                                            .Register(*js::MakeProperty("key", ReadBlendShapeKeys, &BlendShape::mKeys))
+                                            .Register(*new js::Property<BlendShape, std::string_view>("name", js::Read::StringView, &BlendShape::mNodeName))
+                                            .Register(*js::MakeProperty("morphtarget", js::Read::Number<uint32_t>, &BlendShape::mNumberOfMorphTarget))
+                                            .Register(*new js::Property<BlendShape, std::string_view>("blendShapeVersion", js::Read::StringView, &BlendShape::mVersion))
+                                            .Register(*new js::Property<BlendShape, std::string_view>("fullName", js::Read::StringView, &BlendShape::mFullName))
+                                            .Register(*js::MakeProperty("morphname", js::Read::Array<std::string_view, js::Read::StringView>, &BlendShape::mMorphNames)));
+
+const auto FACIAL_ANIMATION_READER = std::move(js::Reader<FacialAnimation>()
+                                                 .Register(*new js::Property<FacialAnimation, std::string_view>("name", js::Read::StringView, &FacialAnimation::mName))
+                                                 .Register(*js::MakeProperty("blendShapes", js::Read::Array<BlendShape, js::ObjectReader<BlendShape>::Read>, &FacialAnimation::mBlendShapes))
+                                                 .Register(*new js::Property<FacialAnimation, std::string_view>("version", js::Read::StringView, &FacialAnimation::mVersion))
+                                                 .Register(*js::MakeProperty("shapesAmount", js::Read::Number<uint32_t>, &FacialAnimation::mNumberOfShapes))
+                                                 .Register(*js::MakeProperty("time", js::Read::Array<uint32_t, js::Read::Number>, &FacialAnimation::mTime))
+                                                 .Register(*js::MakeProperty("frames", js::Read::Number<uint32_t>, &FacialAnimation::mNumberOfFrames)));
+
+} // unnamed namespace
+
+namespace SceneLoader
+{
+AnimationDefinition LoadFacialAnimation(const std::string& url)
+{
+  bool failed = false;
+  auto js     = LoadTextFile(url.c_str(), &failed);
+  if(failed)
+  {
+    ExceptionFlinger(ASSERT_LOCATION) << "Failed to load " << url << ".";
+  }
+
+  json::unique_ptr root(json_parse(js.c_str(), js.size()));
+  if(!root)
+  {
+    ExceptionFlinger(ASSERT_LOCATION) << "Failed to parse " << url << ".";
+  }
+
+  static bool setObjectReaders = true;
+  if(setObjectReaders)
+  {
+    // NOTE: only referencing own, anonymous namespace, const objects; the pointers will never need to change.
+    js::SetObjectReader(BLEND_SHAPE_READER);
+    setObjectReaders = false;
+  }
+
+  auto& rootObj = js::Cast<json_object_s>(*root);
+
+  FacialAnimation facialAnimation;
+  FACIAL_ANIMATION_READER.Read(rootObj, facialAnimation);
+
+  AnimationDefinition animationDefinition;
+  animationDefinition.mName     = std::string(facialAnimation.mName.data());
+  animationDefinition.mDuration = MILLISECONDS_TO_SECONDS * static_cast<float>(facialAnimation.mTime[facialAnimation.mNumberOfFrames - 1u]);
+
+  // Calculate the number of animated properties.
+  uint32_t numberOfAnimatedProperties = 0u;
+  for(const auto& blendShape : facialAnimation.mBlendShapes)
+  {
+    numberOfAnimatedProperties += blendShape.mNumberOfMorphTarget;
+  }
+  animationDefinition.mProperties.resize(numberOfAnimatedProperties);
+
+  // Create the key frame instances.
+  for(auto& animatedProperty : animationDefinition.mProperties)
+  {
+    animatedProperty.mKeyFrames = Dali::KeyFrames::New();
+  }
+
+  // Set the property names
+  char        weightNameBuffer[32];
+  char* const pWeightName = weightNameBuffer + sprintf(weightNameBuffer, "%s", BlendShapes::WEIGHTS_UNIFORM.c_str());
+  uint32_t    targets     = 0u;
+  for(const auto& blendShape : facialAnimation.mBlendShapes)
+  {
+    for(uint32_t morphTargetIndex = 0u; morphTargetIndex < blendShape.mNumberOfMorphTarget; ++morphTargetIndex)
+    {
+      AnimatedProperty& animatedProperty = animationDefinition.mProperties[targets + morphTargetIndex];
+      animatedProperty.mTimePeriod       = Dali::TimePeriod(animationDefinition.mDuration);
+
+      animatedProperty.mNodeName = blendShape.mNodeName;
+
+      snprintf(pWeightName, sizeof(weightNameBuffer) - (pWeightName - weightNameBuffer), "[%d]", morphTargetIndex);
+      animatedProperty.mPropertyName = weightNameBuffer;
+    }
+    targets += blendShape.mNumberOfMorphTarget;
+  }
+
+  targets = 0u;
+  for(const auto& blendShape : facialAnimation.mBlendShapes)
+  {
+    for(uint32_t timeIndex = 0u; timeIndex < facialAnimation.mNumberOfFrames; ++timeIndex)
+    {
+      const float progress = MILLISECONDS_TO_SECONDS * static_cast<float>(facialAnimation.mTime[timeIndex]) / animationDefinition.mDuration;
+
+      for(uint32_t morphTargetIndex = 0u; morphTargetIndex < blendShape.mNumberOfMorphTarget; ++morphTargetIndex)
+      {
+        AnimatedProperty& animatedProperty = animationDefinition.mProperties[targets + morphTargetIndex];
+
+        animatedProperty.mKeyFrames.Add(progress, blendShape.mKeys[timeIndex][morphTargetIndex]);
+      }
+    }
+    targets += blendShape.mNumberOfMorphTarget;
+  }
+
+  return animationDefinition;
+}
+
+} // namespace SceneLoader
+} // namespace Dali
