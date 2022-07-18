385560bb0a9ceecdeaba9e80cfe5491dee178efd
Heeyong Song <heeyong.song@samsung.com>
2022-07-18 18:19:11 +0900

    [Tizen](Vector) Change SetPlayRange
    Change-Id: Icbff96676db3e3c99fab067b45588cb4ff9cc271

--- dali-toolkit/internal/visuals/animated-vector-image/vector-animation-task.cpp
+++ dali-toolkit/internal/visuals/animated-vector-image/vector-animation-task.cpp
@@ -280,38 +280,33 @@ void VectorAnimationTask::SetPlayRange(const Property::Array& playRange)
   }
 
   // Make sure the range specified is between 0 and the total frame number
-  if(startFrame < mTotalFrame && endFrame < mTotalFrame)
+  startFrame = std::min(startFrame, mTotalFrame - 1);
+  endFrame   = std::min(endFrame, mTotalFrame - 1);
+
+  // If the range is not in order swap values
+  if(startFrame > endFrame)
   {
-    // If the range is not in order swap values
-    if(startFrame > endFrame)
-    {
-      uint32_t temp = startFrame;
-      startFrame    = endFrame;
-      endFrame      = temp;
-    }
-
-    if(startFrame != mStartFrame || endFrame != mEndFrame)
-    {
-      mStartFrame = startFrame;
-      mEndFrame   = endFrame;
-
-      // If the current frame is out of the range, change the current frame also.
-      if(mStartFrame > mCurrentFrame)
-      {
-        mCurrentFrame = mStartFrame;
-      }
-      else if(mEndFrame < mCurrentFrame)
-      {
-        mCurrentFrame = mEndFrame;
-      }
-
-      DALI_LOG_INFO(gVectorAnimationLogFilter, Debug::Verbose, "VectorAnimationTask::SetPlayRange: [%d, %d] [%s] [%p]\n", mStartFrame, mEndFrame, mUrl.c_str(), this);
-    }
+    uint32_t temp = startFrame;
+    startFrame    = endFrame;
+    endFrame      = temp;
   }
-  else
+
+  if(startFrame != mStartFrame || endFrame != mEndFrame)
   {
-    DALI_LOG_ERROR("VectorAnimationTask::SetPlayRange: Invalid range (%d, %d) [%s] [%p]\n", startFrame, endFrame, mUrl.c_str(), this);
-    return;
+    mStartFrame = startFrame;
+    mEndFrame   = endFrame;
+
+    // If the current frame is out of the range, change the current frame also.
+    if(mStartFrame > mCurrentFrame)
+    {
+      mCurrentFrame = mStartFrame;
+    }
+    else if(mEndFrame < mCurrentFrame)
+    {
+      mCurrentFrame = mEndFrame;
+    }
+
+    DALI_LOG_INFO(gVectorAnimationLogFilter, Debug::Verbose, "VectorAnimationTask::SetPlayRange: [%d, %d] [%s] [%p]\n", mStartFrame, mEndFrame, mUrl.c_str(), this);
   }
 }
 
