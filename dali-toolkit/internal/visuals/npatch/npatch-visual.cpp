8d79ff4c31a162540ba8a3bd026fc6a3a0f23fa5
Eunki Hong <eunkiki.hong@samsung.com>
2023-08-10 16:41:22 +0900

    Allow npatch image load remote url
    Since we don't need to ignore remote npatch image, let we remove
    url validation check code.
    
    (And, also that code doesn't show broken image)
    
    Change-Id: I37900863f64a68abb66f6c8a7fd3e21719896ba7
    Signed-off-by: Eunki Hong <eunkiki.hong@samsung.com>

--- dali-toolkit/internal/visuals/npatch/npatch-visual.cpp
+++ dali-toolkit/internal/visuals/npatch/npatch-visual.cpp
@@ -74,7 +74,7 @@ void NPatchVisual::LoadImages()
   TextureManager& textureManager     = mFactoryCache.GetTextureManager();
   bool            synchronousLoading = mImpl->mFlags & Impl::IS_SYNCHRONOUS_RESOURCE_LOADING;
 
-  if(mId == NPatchData::INVALID_NPATCH_DATA_ID && (mImageUrl.IsLocalResource() || mImageUrl.IsBufferResource()))
+  if(mId == NPatchData::INVALID_NPATCH_DATA_ID)
   {
     bool preMultiplyOnLoad = IsPreMultipliedAlphaEnabled() && !mImpl->mCustomShader ? true : false;
     mId                    = mLoader.Load(textureManager, this, mImageUrl, mBorder, preMultiplyOnLoad, synchronousLoading);
@@ -86,7 +86,7 @@ void NPatchVisual::LoadImages()
     }
   }
 
-  if(mAuxiliaryTextureId == TextureManager::INVALID_TEXTURE_ID && mAuxiliaryUrl.IsValid() && (mAuxiliaryUrl.IsLocalResource() || mAuxiliaryUrl.IsBufferResource()))
+  if(mAuxiliaryTextureId == TextureManager::INVALID_TEXTURE_ID && mAuxiliaryUrl.IsValid())
   {
     auto preMultiplyOnLoad = IsPreMultipliedAlphaEnabled() && !mImpl->mCustomShader
                                ? TextureManager::MultiplyOnLoad::MULTIPLY_ON_LOAD
