11c59a90d2751eedeb979707864580682ad9179f
Eunki, Hong <eunkiki.hong@samsung.com>
2023-08-29 15:52:47 +0900

    Guard NPatchData removal case during signal emit
    It was possible that NPatchData removed during NotifyObserver.
    For more safety, let we make NPatchData as shared_ptr, instead
    of unique_ptr.
    
    And also, let we don't touch observer list container during
    NotifyObservers.
    
    And also, let we remove NPatchInfo at post processing.
    It will keep the life of NPatchData during NotifyObserver try to
    remove the NPatchInfo.
    
    Change-Id: Ieca33a2231df38d23966f96593506d67333cfcd0
    Signed-off-by: Eunki, Hong <eunkiki.hong@samsung.com>

--- dali-toolkit/internal/visuals/npatch-loader.h
+++ dali-toolkit/internal/visuals/npatch-loader.h
@@ -2,7 +2,7 @@
 #define DALI_TOOLKIT_NPATCH_LOADER_H
 
 /*
- * Copyright (c) 2022 Samsung Electronics Co., Ltd.
+ * Copyright (c) 2023 Samsung Electronics Co., Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,9 +19,11 @@
 
 // EXTERNAL INCLUDES
 #include <dali/devel-api/adaptor-framework/pixel-buffer.h>
+#include <dali/integration-api/processor-interface.h>
 #include <dali/public-api/rendering/texture-set.h>
-#include <memory> // for std::unique_ptr
+#include <memory> // for std::shared_ptr
 #include <string>
+#include <utility> // for std::pair
 
 // INTERNAL INCLUDES
 #include <dali-toolkit/devel-api/utility/npatch-utilities.h>
@@ -44,7 +46,7 @@ namespace Internal
  * small space and there's not usually a lot of them. Usually N patches are specified in
  * toolkit default style and there is 1-2 per control that are shared across the whole application.
  */
-class NPatchLoader
+class NPatchLoader : public Integration::Processor
 {
 public:
   /**
@@ -69,16 +71,7 @@ public:
    * @param [in] synchronousLoading True if the image will be loaded in synchronous time.
    * @return id of the texture.
    */
-  std::size_t Load(TextureManager& textureManager, TextureUploadObserver* textureObserver, const VisualUrl& url, const Rect<int>& border, bool& preMultiplyOnLoad, bool synchronousLoading);
-
-  /**
-   * @brief Set loaded PixelBuffer and its information
-   *
-   * @param [in] id cache data id
-   * @param [in] pixelBuffer of loaded image
-   * @param [in] preMultiplied True if the image had pre-multiplied alpha applied
-   */
-  void SetNPatchData(std::size_t id, Devel::PixelBuffer& pixelBuffer, bool preMultiplied);
+  NPatchData::NPatchDataId Load(TextureManager& textureManager, TextureUploadObserver* textureObserver, const VisualUrl& url, const Rect<int>& border, bool& preMultiplyOnLoad, bool synchronousLoading);
 
   /**
    * @brief Retrieve N patch data matching to an id
@@ -89,6 +82,26 @@ public:
   bool GetNPatchData(const NPatchData::NPatchDataId id, const NPatchData*& data);
 
   /**
+   * @brief Request remove a texture matching id.
+   * Erase the observer from the observer list of cache if we need.
+   *
+   * @param [in] id cache data id
+   * @param [in] textureObserver The NPatchVisual that requested loading.
+   */
+  void RequestRemove(NPatchData::NPatchDataId id, TextureUploadObserver* textureObserver);
+
+protected: // Implementation of Processor
+  /**
+   * @copydoc Dali::Integration::Processor::Process()
+   */
+  void Process(bool postProcessor) override;
+
+private:
+  NPatchData::NPatchDataId GenerateUniqueNPatchDataId();
+
+  int32_t GetCacheIndexFromId(const NPatchData::NPatchDataId id);
+
+  /**
    * @brief Remove a texture matching id.
    * Erase the observer from the observer list of cache if we need.
    * This API decrease cached NPatchInfo reference.
@@ -97,12 +110,7 @@ public:
    * @param [in] id cache data id
    * @param [in] textureObserver The NPatchVisual that requested loading.
    */
-  void Remove(std::size_t id, TextureUploadObserver* textureObserver);
-
-private:
-  NPatchData::NPatchDataId GenerateUniqueNPatchDataId();
-
-  int32_t GetCacheIndexFromId(const NPatchData::NPatchDataId id);
+  void Remove(NPatchData::NPatchDataId id, TextureUploadObserver* textureObserver);
 
 private:
   /**
@@ -111,7 +119,7 @@ private:
    */
   struct NPatchInfo
   {
-    NPatchInfo(NPatchData* data)
+    NPatchInfo(std::shared_ptr<NPatchData> data)
     : mData(data),
       mReferenceCount(1u)
     {
@@ -137,7 +145,7 @@ private:
     NPatchInfo(const NPatchInfo& info) = delete;            // Do not use copy constructor
     NPatchInfo& operator=(const NPatchInfo& info) = delete; // Do not use copy assign
 
-    std::unique_ptr<NPatchData> mData;
+    std::shared_ptr<NPatchData> mData;
     std::int16_t                mReferenceCount; ///< The number of N-patch visuals that use this data.
   };
 
@@ -151,7 +159,7 @@ private:
    *                                   image has no alpha channel
    * @return NPatchData pointer that Load function will used.
    */
-  NPatchData* GetNPatchData(const VisualUrl& url, const Rect<int>& border, bool& preMultiplyOnLoad);
+  std::shared_ptr<NPatchData> GetNPatchData(const VisualUrl& url, const Rect<int>& border, bool& preMultiplyOnLoad);
 
 protected:
   /**
@@ -167,6 +175,10 @@ protected:
 private:
   NPatchData::NPatchDataId mCurrentNPatchDataId;
   std::vector<NPatchInfo>  mCache;
+
+  std::vector<std::pair<NPatchData::NPatchDataId, TextureUploadObserver*>> mRemoveQueue; ///< Queue of textures to remove at PostProcess. It will be cleared after PostProcess.
+
+  bool mRemoveProcessorRegistered : 1; ///< Flag if remove processor registered or not.
 };
 
 } // namespace Internal
