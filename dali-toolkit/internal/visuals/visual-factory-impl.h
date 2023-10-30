fd1b36f430f9a51b421d2b908fb1391e580c8229
Taehyub Kim <taehyub.kim@samsung.com>
2023-10-30 19:13:28 +0900

    Revert "[Tizen] Change precompile shader list for performance"
    This reverts commit 575395b7c3c37e49a910ad141b3cd0f4b037ae98.

--- dali-toolkit/internal/visuals/visual-factory-impl.h
+++ dali-toolkit/internal/visuals/visual-factory-impl.h
@@ -115,12 +115,6 @@ private:
   void SetBrokenImageUrl(Toolkit::StyleManager& styleManager);
 
   /**
-   * @brief Get the default shader source.
-   * @param[in] shaders shaderList for precompile
-   */
-  void GetPrecompiledShader(RawShaderData& shaders);
-
-  /**
    * Get the factory cache, creating it if necessary.
    */
   Internal::VisualFactoryCache& GetFactoryCache();
