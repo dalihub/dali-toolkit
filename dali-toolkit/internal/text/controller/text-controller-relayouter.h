a316b9ea7fcf64a53c632fe7a53cada580dc7a00
ANZ1217 <chihun.jeong@samsung.com>
2024-01-03 15:51:34 +0900

    [Tizen] Fix TextFit do not work properly
    When check for textfit, it doesn't distinguish between cases
    where the layout does not change because text is too large
    and the layout does not change because text is too small.
    
    Now CheckForTextFit returns false when text is too large.
    
    Change-Id: I03a414406ee336ec7bf24c8d067fc73379d119f7

--- dali-toolkit/internal/text/controller/text-controller-relayouter.h
+++ dali-toolkit/internal/text/controller/text-controller-relayouter.h
@@ -97,10 +97,23 @@ struct Controller::Relayouter
    * @param[in] size The size to set
    * @param[in] operationsRequired The operations we need to do
    * @param[in/out] layoutSize The Layout size which can be updated depending on the result of the performed operations
-   * @return
+   * @return whether view updated after relayout
    */
 
   static bool DoRelayout(Controller::Impl& impl, const Size& size, OperationsMask operationsRequired, Size& layoutSize);
+
+  /**
+   * @brief Called by the Controller to do certain operations when relayouting.
+   *
+   * @param[in] impl A reference to the controller impl class
+   * @param[in] size The size to set
+   * @param[in] operationsRequired The operations we need to do
+   * @param[in/out] layoutSize The Layout size which can be updated depending on the result of the performed operations
+   * @param[out] layoutTooSmall True if layout is too small to render one glyph, else false.
+   * @return whether view updated after relayout
+   */
+
+  static bool DoRelayout(Controller::Impl& impl, const Size& size, OperationsMask operationsRequired, Size& layoutSize, bool& layoutTooSmall);
 
   /**
    * @brief Called by the Controller to calculate the veritcal offset give the control size.
