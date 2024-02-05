1a357dda85bd4ea90c9ba7a467c63a4a1a45f1c9
Eunki, Hong <eunkiki.hong@samsung.com>
2024-02-05 19:06:34 +0900

    Ignore corner blur radius if glsl version is low
    Let we make to not blur rounded corner if gles version is too low
    
    Change-Id: I1ed43633d97e63a0b4d843ce692e7db28db22e0d
    Signed-off-by: Eunki, Hong <eunkiki.hong@samsung.com>

--- dali-toolkit/internal/visuals/color/color-visual.cpp
+++ dali-toolkit/internal/visuals/color/color-visual.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Samsung Electronics Co., Ltd.
+ * Copyright (c) 2024 Samsung Electronics Co., Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -62,6 +62,8 @@ enum ColorVisualRequireFlag
   BORDERLINE     = 1 << 1,
   BLUR           = 1 << 2,
 };
+
+constexpr uint32_t MINIMUM_SHADER_VERSION_SUPPORT_ROUNDED_BLUR = 300;
 } // unnamed namespace
 ColorVisualPtr ColorVisual::New(VisualFactoryCache& factoryCache, const Property::Map& properties)
 {
@@ -235,15 +237,21 @@ Shader ColorVisual::GenerateShader() const
   bool blur           = IsBlurRequired();
   int  shaderTypeFlag = ColorVisualRequireFlag::DEFAULT;
 
-  if(roundedCorner)
-  {
-    shaderTypeFlag |= ColorVisualRequireFlag::ROUNDED_CORNER;
-  }
   if(blur)
   {
     // If we use blur, just ignore borderline
     borderline = false;
     shaderTypeFlag |= ColorVisualRequireFlag::BLUR;
+
+    // If shader version doesn't support blur with corner radius, ignore corner radius
+    if(DALI_UNLIKELY(Dali::Shader::GetShaderLanguageVersion() < MINIMUM_SHADER_VERSION_SUPPORT_ROUNDED_BLUR))
+    {
+      roundedCorner = false;
+    }
+  }
+  if(roundedCorner)
+  {
+    shaderTypeFlag |= ColorVisualRequireFlag::ROUNDED_CORNER;
   }
   if(borderline)
   {
