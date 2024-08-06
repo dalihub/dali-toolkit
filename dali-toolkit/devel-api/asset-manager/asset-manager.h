00adb6e1d991b11db9645e7fa3448127ade8af21
Eunki, Hong <eunkiki.hong@samsung.com>
2024-08-06 10:58:05 +0900

    [Tizen] Ensure release some Texture/Geometry handles after adaptor stop
    Since off-screen application could be re-started without process terminate,
    we should not keep some dali resources at static area.
    
    Change-Id: Idee042f408e3cf7dd3b026b9f555ba9896cd270a
    Signed-off-by: Eunki, Hong <eunkiki.hong@samsung.com>

--- dali-toolkit/devel-api/asset-manager/asset-manager.h
+++ dali-toolkit/devel-api/asset-manager/asset-manager.h
@@ -2,7 +2,7 @@
 #define DALI_TOOLKIT_ASSET_MANAGER_DEVEL_H
 
 /*
- * Copyright (c) 2020 Samsung Electronics Co., Ltd.
+ * Copyright (c) 2024 Samsung Electronics Co., Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,6 +19,7 @@
  */
 
 // EXTERNAL INCLUDES
+#include <dali-toolkit/public-api/dali-toolkit-common.h>
 #include <string>
 
 // INTERNAL INCLUDES
@@ -30,7 +31,7 @@ namespace Toolkit
 /**
  * @brief Retrieves the file system path of the assets.
  */
-class AssetManager
+class DALI_TOOLKIT_API AssetManager
 {
 public:
   /**
