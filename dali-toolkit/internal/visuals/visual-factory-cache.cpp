73ee4312b7a582a52b0eed0d492333eac1e4b3ee
sunghyun kim <scholb.kim@samsung.com>
2025-04-14 13:32:10 +0900

    Added FILE_CACHE_SUPPORT Hint for shader
    please refer to dali-core patch
    : https://review.tizen.org/gerrit/c/platform/core/uifw/dali-core/+/322649
    
    Change-Id: I6e6f1dd61064be5f514d7e80f8af5d2cdd63f688

--- dali-toolkit/internal/visuals/visual-factory-cache.cpp
+++ dali-toolkit/internal/visuals/visual-factory-cache.cpp
@@ -102,16 +102,22 @@ Shader VisualFactoryCache::GetShader(ShaderType type, bool useDefaultUniforms)
 Shader VisualFactoryCache::GenerateAndSaveShader(ShaderType type, std::string_view vertexShader, std::string_view fragmentShader, bool useDefaultUniforms)
 {
   Shader shader;
+  std::string shaderName = Scripting::GetLinearEnumerationName<ShaderType>(type, VISUAL_SHADER_TYPE_TABLE, VISUAL_SHADER_TYPE_TABLE_COUNT);
+
+  // If the shader name is empty, it means that the shader is not generated internally. So, there is need to support file caching. Otherwise, it is defined externally. So, it needs not to support file caching.
+  Shader::Hint::Value shaderHints = shaderName.empty()? Shader::Hint::NONE : Shader::Hint::FILE_CACHE_SUPPORT;
+
   if(useDefaultUniforms)
   {
-    shader               = Integration::ShaderNewWithUniformBlock(vertexShader, fragmentShader, Shader::Hint::NONE, Scripting::GetLinearEnumerationName<ShaderType>(type, VISUAL_SHADER_TYPE_TABLE, VISUAL_SHADER_TYPE_TABLE_COUNT), {GetDefaultUniformBlock()});
+    shader               = Integration::ShaderNewWithUniformBlock(vertexShader, fragmentShader, shaderHints, shaderName, {GetDefaultUniformBlock()});
     mDefaultShader[type] = shader;
   }
   else
   {
-    shader        = Shader::New(vertexShader, fragmentShader, Shader::Hint::NONE, Scripting::GetLinearEnumerationName<ShaderType>(type, VISUAL_SHADER_TYPE_TABLE, VISUAL_SHADER_TYPE_TABLE_COUNT));
+    shader = Shader::New(vertexShader, fragmentShader, shaderHints, shaderName);
     mShader[type] = shader;
   }
+
   return shader;
 }
 
