805ccf024c800e1389d94917ef48c6636dda74f7
joogab.yun <joogab.yun@samsung.com>
2025-09-03 14:14:53 +0900

    Revert "Add AutoRotation, LetterBox"
    This needs to be reimplemented.
    
    Change-Id: I0c61d895935f632b5cb143dfedca76c2f91d9ca7

--- dali-toolkit/internal/controls/camera-view/camera-view-impl.cpp
+++ dali-toolkit/internal/controls/camera-view/camera-view-impl.cpp
@@ -189,10 +189,6 @@ Dali::Shader CameraView::CreateShader(Dali::NativeImageSourcePtr nativeImageSour
   nativeImageSourcePtr->ApplyNativeFragmentShader(fragmentShader, 1);
 
   Dali::Shader shader = Dali::Shader::New(vertexShader, fragmentShader, static_cast<Shader::Hint::Value>(Shader::Hint::FILE_CACHE_SUPPORT | Shader::Hint::INTERNAL), "CAMERA_VIEW");
-  // Initialize shader properties
-  shader.RegisterProperty("uRotationMatrix", Property::Value(Vector4(1.0f, 0.0f, 0.0f, 1.0f)));
-  shader.RegisterProperty("uSizeRatio", Property::Value(Vector2(0.0f, 0.0f)));
-
   return shader;
 }
 
