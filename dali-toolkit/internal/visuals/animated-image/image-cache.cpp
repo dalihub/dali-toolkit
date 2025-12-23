484c66a10d8437fe1659d5667aa63477775841f3
Eunki Hong <eunkiki.hong@samsung.com>
2025-12-23 15:02:35 +0900

    (AnimatedImageVisual) Support some mordern properties for ImageVisual
    Let we clean the codes, and support some new features of ImageVisual to AnimattedImageVisual
    - EnableBrokenImage
    - SynchronousSizing
    
    Change-Id: I459df07f77ca4cb592743c4da05a88afb8e431eb
    Signed-off-by: Eunki Hong <eunkiki.hong@samsung.com>

--- dali-toolkit/internal/visuals/animated-image/image-cache.cpp
+++ dali-toolkit/internal/visuals/animated-image/image-cache.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Samsung Electronics Co., Ltd.
+ * Copyright (c) 2025 Samsung Electronics Co., Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -53,6 +53,13 @@ ImageCache::~ImageCache()
 void ImageCache::SetInterval(uint32_t interval)
 {
   mInterval = interval;
+}
+
+void ImageCache::SetDesiredSize(Dali::ImageDimensions desiredSize)
+{
+  mDesiredSize = desiredSize;
+
+  ClearCache();
 }
 
 } //namespace Internal
