91dc49561f9de9c38760324559185bb26e20d2f3
huayongxu <huayong.xu@samsung.com>
2026-01-26 09:44:14 +0800

    Change enum Type to enum class Type in OffScreenRenderable.
    Option None in OffScreenRenderable::Type is very common.
    It might conflict with the ones in other common headers.
    
    Change-Id: I135b9dc8d5cf3d1b4edffdae02d6adfd700b6753

--- dali-toolkit/internal/controls/render-effects/mask-effect-impl.cpp
+++ dali-toolkit/internal/controls/render-effects/mask-effect-impl.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025 Samsung Electronics Co., Ltd.
+ * Copyright (c) 2026 Samsung Electronics Co., Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -102,7 +102,7 @@ MaskEffectImplPtr MaskEffectImpl::New(Toolkit::Control maskControl, MaskEffect::
 
 OffScreenRenderable::Type MaskEffectImpl::GetOffScreenRenderableType()
 {
-  return OffScreenRenderable::FORWARD;
+  return OffScreenRenderable::Type::FORWARD;
 }
 
 void MaskEffectImpl::GetOffScreenRenderTasks(std::vector<Dali::RenderTask>& tasks, bool isForward)
